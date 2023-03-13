fbda35d7047b876e03e1c4d6ee4d45b1df4546f0
Haitian Xu <89272253+Tonymisaka@users.noreply.github.com>
2023-03-12 21:07:49 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let doge = document.querySelector(".dogem");
 document.addEventListener("mousemove", function(e){
-    doge.style.left = e.pageX + 20 + "px";
-    doge.style.top = e.pageY + 20+ "px";
+    doge.style.left = e.clientX + 20 + "px";
+    doge.style.top = e.clientY + 20+ "px";
 });
